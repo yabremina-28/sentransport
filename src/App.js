24c3cced4b79695ff51bcddac5ca6bd8236c1d5b
Amina Yabre <yabremina@gmail.com>
2026-05-02 13:09:40 +0200

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import ListeLignes from './ListeLignes';
+import Footer from './Footer';
 
 function App() {
+  const lignes = [
+    { id: 1, numero: "1", depart: "Parcelles Assainies", arrivee: "Plateau", arrets: 14 },
+    { id: 2, numero: "7", depart: "Guediawaye", arrivee: "Place Obe", arrets: 18 },
+    { id: 3, numero: "15", depart: "Pikine", arrivee: "Medina", arrets: 12 },
+    { id: 4, numero: "23", depart: "Ouakam", arrivee: "Grand Dakar", arrets: 10 },
+    { id: 5, numero: "8", depart: "Almadies", arrivee: "Colobane", arrets: 16 },
+    { id: 6, numero: "12", depart: "Yoff", arrivee: "Sandaga", arrets: 11 },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="contenu">
+        <ListeLignes lignes={lignes} />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2026 SenTransport &mdash; ESP / UCAD</p>
+    </footer>
+  );
+}
+
+export default Footer;
